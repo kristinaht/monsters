ee844669ade0d2f2bf07bc0685ae84f4faceee62
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-08-29 15:31:55 -0700

    create card-list component and display all monsters as props.children, with help of grid in css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { CardList } from './components/card-list/card-list.component'
+
 class App extends Component {
   constructor() {
     super();
@@ -18,7 +20,9 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        {this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)}
+         <CardList name={'Kristina'}>
+           {this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)}
+         </CardList>
       </div>
     );
   }
