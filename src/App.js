1d4ac0ab51335f1a305ec824b47350ab2250a951
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-08-30 16:45:04 -0700

    create a functioning searchField: use destructuring within render method, create filteredMonsters array and pass them to the CardList

--- src/App.js
+++ src/App.js
@@ -19,15 +19,17 @@ class App extends Component {
       .then(users => this.setState({monsters: users}));
   }
   render(){
+    const { monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()) )
+    
     return (
       <div className="App">
         <input 
           type='search' 
           placeholder='search monsters' 
           onChange={e => {
-            this.setState({ searchField: e.target.value}, () => console.log(this.state)); 
-            } } />
-        <CardList monsters={this.state.monsters}/>
+            this.setState({ searchField: e.target.value }); }} />
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
@@ -38,4 +40,6 @@ export default App;
 //setState in onChange:
 //because setState is async function, the searchField is one letter behind. In order to fix this, I pass another argument to setState - I pass in the callback function
 
-//remember: if you want to do something with the state right after you set it, you have to do it inside the second argument function within setSTate function. This callback f is called right after setState
+//setState doesn't update the DOM right away. We declare that the DOM needs to be updated, but React calculates the best time to actually update the DOM.
+
+//REMEMBER: if you want to do something with the state right after you set it, you have to do it inside the second argument function within setSTate function. This callback f is called right after setState
