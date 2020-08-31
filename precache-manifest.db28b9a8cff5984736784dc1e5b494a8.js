88d63925701818067e186ec06ed8083761a80d56
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-08-30 17:57:33 -0700

    Updates

---
+++ precache-manifest.db28b9a8cff5984736784dc1e5b494a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb9d5480bc9b9891dbad2b4750569d0",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e9f432cd90836b42a992",
+    "url": "/monsters/static/css/main.393dfee1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f432cd90836b42a992",
+    "url": "/monsters/static/js/main.b815d289.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
