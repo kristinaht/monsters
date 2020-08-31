b360a7e1d0ef6b898cde7112e98ea85d15998a0f
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-08-30 17:33:56 -0700

    refactor App.js to create reusable handleChange arrow function

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({monsters: users}));
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
   render(){
     const { monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()) )
@@ -28,8 +32,7 @@ class App extends Component {
       <div className="App">
         <SearchBox
           placeholder='search monsters'
-          handleChange= {e => {
-            this.setState({ searchField: e.target.value }); }} />
+          handleChange= {this.handleChange} />
         <CardList monsters={filteredMonsters}/>
       </div>
     );
